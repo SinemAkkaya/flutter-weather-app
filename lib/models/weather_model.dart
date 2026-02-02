b3628e97bf1e830110e99074ce43d23cd39269a1
SinemAkkaya <sinemakkayamail@icloud.com>
2026-02-02 22:39:48 +0300

    Create WeatherModel with custom learning notes

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,27 @@
+class WeatherModel {
+  final String cityName;
+  final double temperature;
+  final String description;
+  final String iconCode;
+
+//Constructor: bu alanlar zorunludur (required), Swift'teki gibi boş (nil) olamazlar. (optional ın tersi )
+//gelen veriyi direkt değişkene koy demek için this. kullanıyorum
+  WeatherModel({
+    required this.cityName, //this.cityName = cityName demeden kısaca yazabilirim 
+    required this.temperature,
+    required this.description,
+    required this.iconCode,
+  });
+  
+ // Factory Constructor: JSON verisini alıp Model nesnesine çeviren yapı ( swift'teki 'Decoder' burada manuel yapılıyor.)
+  factory WeatherModel.fromJson(Map<String, dynamic> json) { // dynamic herhangi bir tip olabilir demek (any gibi)
+    return WeatherModel(
+      cityName: json['name'],
+
+      //API'den gelen sıcaklık eğer tam sayı (int) olmazsa diye önce 'num' olarak alıp 'double'a çevirmeliyim
+      temperature: (json['main']['temp'] as num ).toDouble(), // swiftte noktayla yapılırdı dartta etiketle yapılıyor bunu unutma json.main.temp --> json['main']['temp']
+      description: json['weather'][0]['description'], 
+      iconCode: json['weather'][0]['icon'],
+    );
+  }
+}
